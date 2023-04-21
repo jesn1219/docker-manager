8b8c0cb5c9daceae5fd9d45484430d9219663846
jesn1219 <jesn1219@gmail.com>
2023-04-21 08:52:56 +0000

    230421

--- venv_mujoco_odt/commands.sh
+++ venv_mujoco_odt/commands.sh
@@ -1,3 +1,4 @@
 docker build -t jesn1219/odt:latest .
 
-docker run -it --gpus al -e DISPLAY=$DISPLAY -p 60062:6006 -p 22002:2202 jesn1219/odt bash
+docker run -it --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -p 60061:6006 -p 22001:22 jesn1219/dt bash
+docker run -it --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -p 60061:6006 -p 22001:22 --name dt2 jesn1219/dt bash
