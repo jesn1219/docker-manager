da27b22e2a9d06a3f607669b49c5368416b8319b
jesn1219 <jesn1219@gmail.com>
2023-06-20 17:25:23 +0900

    230620

---
+++ setup.sh
@@ -0,0 +1,13 @@
+passwd;
+cd /research;
+git clone https://github.com/jesn1219/agent-playground;
+git clone https://github.com/jesn1219/jesnk_packages;
+
+
+cd /research/agent-playground;
+git submodule update --init --recursive;
+
+
+
+
+
