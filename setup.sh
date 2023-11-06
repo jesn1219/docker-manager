b6410759c7a202ecad73278bc8b9be30741e6038
jesn1219 <jesn1219@gmail.com>
2023-11-06 23:53:53 +0900

    setup.sh modified

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-sudo ./scripts/change_ssh_port.sh 
+sh ./scripts/change_ssh_port.sh 
 passwd;
 
 cd /research;
 git clone https://github.com/jesn1219/jesnk_packages;
-
+sv2 && cd jesnk_packages && sh setup.sh && sv2
+wandb init;
 
 
 
