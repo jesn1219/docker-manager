3c7d987ff2c013232fab92bdc7019c8002d508c4
jesn1219 <jesn1219@gmail.com>
2023-11-11 15:24:15 +0000

    gpu_check edited

--- gpu_check.py
+++ gpu_check.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 from jesnk_utils.telebot import Telebot
+import sys
 
 alpha = Telebot()
 
@@ -12,14 +13,24 @@ def get_gpu_memory():
     gpu_memory_used = int(result.stdout.strip())
     return gpu_memory_used
 
-# Loop to continuously check GPU memory usage
-while True:
+
+if __name__ == '__main__' :
+    message = f'GPU memory check module started'
+    alpha.send_message(message)
     gpu_memory_used = get_gpu_memory()
-    # Check if the GPU memory used is less than 500MB
-    if gpu_memory_used < 500 :
-        current_memory_str = str(gpu_memory_used)
-        message = f'GPU memory usage is {current_memory_str}MB'
-        alpha.send_message(message)
-        break
-    time.sleep(3)  # Wait for 1 second before the next check
+    current_memory_str = str(gpu_memory_used)
+    message = f'GPU memory usage is {current_memory_str}MB'
+    alpha.send_message(message)
+
+    # Loop to continuously check GPU memory usage
+    while True:
+        gpu_memory_used = get_gpu_memory()
+        # Check if the GPU memory used is less than 500MB
+        if gpu_memory_used < 8000 :
+            current_memory_str = str(gpu_memory_used)
+            message = f'GPU memory usage is {current_memory_str}MB'
+            alpha.send_message(message)
+            sys.exit(0)
+            break
+        time.sleep(3)  # Wait for 1 second before the next check
 
