81ca453026bc3e94f793b23e598b4d7012d59887
jesn1219 <jesn1219@gmail.com>
2023-12-19 12:39:04 +0900

    231219

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@ passwd;
 
 cd /research;
 git clone https://github.com/jesn1219/jesnk_packages;
-sv2 && cd jesnk_packages && sh setup.sh && sv2
+source /env2/bin/activate && cd jesnk_packages && sh setup.sh && source /env2/bin/activate
+
+cd /research/rl_zoo_jesnk;
+source /env2/bin/activate && pip uninstall -y stable-baselines3;
+git clone https://github.com/jesnk/stable_baselines3;
+
 wandb init;
 
 
