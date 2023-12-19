04f6321fd1e8f5426e2ee3784ae27e59201437ab
jesn1219 <jesn1219@gmail.com>
2023-12-19 12:12:41 +0900

    231219

--- beta/commands.sh
+++ beta/commands.sh
@@ -1,5 +1,7 @@
 docker build -t jesn1219/beta:latest .
 
+docker run -it --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -p 60061:6006 -p 22001:22 -p 22002:22002 -p 22003:22003 --name beta jesn1219/beta bash
+
 docker run -it --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -p 60061:6006 -p 22001:22 -p 22002:22002 -p 22003:22003 jesn1219/beta bash
 
 docker run -it --gpus all --volume /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY -p 60061:6006 -p 22001:22 -p 22002:22002 -p 22003:22003 jesn1219/beta bash
